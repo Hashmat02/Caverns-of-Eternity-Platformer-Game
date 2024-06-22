49581df95438861f9487e654d924bfd36664e57d
noxayyyy <aisuru.nox@gmail.com>
2024-06-23 04:04:05 +0500

    Add Player Movement & Collisions Functionality & Fix Repo Directory Problems
    - Test Scene
    - Horizontal Movement
    - Jump
    - Double Jump
    - Wall Jump
    - Wall Climb
    - Wall Slide
    - Friction Handling for different surfaces (uses Rigidbody2D.sharedMaterial)
    - Collision Handling
    
    Repo Problems:
    - .gitignore in wrong directory
    - nested folder of the repo

---
+++ Assets/Scripts/CollisionCheck.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour {
+    public bool isGround { get; private set; }
+    public bool isWall { get; private set; }
+    public float friction { get; private set; }
+    public Vector2 contactNormal { get; private set; }
+    [SerializeField, Range(0, 90)] private float _maxAngleWall = 0; // in degrees
+    private PhysicsMaterial2D _material;
+    private float _minWallNormalX;
+
+    void Update() {
+        _minWallNormalX = Mathf.Cos(_maxAngleWall * Mathf.PI / 180.0f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        updateContacts(collision);
+        getFriction(collision);
+    }
+
+    void OnCollisionStay2D(Collision2D collision) {
+        updateContacts(collision);
+        getFriction(collision);
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        isGround = false;
+        isWall = false;
+        friction = 0;
+    }
+
+    void updateContacts(Collision2D collision) {
+        for (int i = 0; i < collision.contactCount; i++) {
+            contactNormal = collision.GetContact(i).normal;
+            isWall |= Mathf.Abs(contactNormal.x) >= _minWallNormalX;
+        }
+        isGround = collision.collider.CompareTag("Ground");
+    }
+
+    void getFriction(Collision2D collision) {
+        _material = collision.rigidbody?.sharedMaterial;
+        friction = 0;
+        if (_material) {
+            friction = _material.friction;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [Header("Movement Properties")]
+    [SerializeField, Range(0.0f, 50.0f)] private float _maxVel = 4.0f;
+    [SerializeField, Range(0.0f, 50.0f)] private float _maxAccel = 40.0f;
+    [SerializeField, Range(0.0f, 50.0f)] private float _maxAirAccel = 20.0f;
+
+    private Vector2 _velocity;
+    private float _moveX;
+    private float _projectedVelX;
+
+    [Header("Jump Properties")]
+    [SerializeField, Range(0.0f, 10.0f)] private float _jumpHeight = 3.0f;
+    [SerializeField, Range(0, 5)] private int _maxAirJumps = 1;
+    [SerializeField, Range(0.0f, 5.0f)] private float _fallMult = 3.0f;
+    [SerializeField, Range(0.0f, 5.0f)] private float _riseMult = 1.5f;
+    [SerializeField, Range(0.0f, 0.5f)] private float _coyoteTime = 0.2f;
+
+    private bool _wantJump = false;
+    private float _coyoteCounter;
+    private int _jumpCount = 0;
+
+    [Header("Wall Interaction Properties")]
+    [SerializeField, Range(0.0f, 5.0f)] private float _wallSlideMaxVel = 2.0f;
+    [SerializeField] private Vector2 _wallJumpClimb = new Vector2(7.0f, 12.0f);
+    [SerializeField] private Vector2 _wallJumpLeap = new Vector2(10.7f, 10.0f);
+    [SerializeField, Range(0.0f, 1.0f)] private float _wallStickTime = 0.5f;
+
+    private bool _wantWallJump = false;
+    private bool _isWallJumping = false;
+    private float _wallStickCounter;
+
+    private PhysicsMaterial2D _material;
+    private Rigidbody2D _body;
+    private CollisionCheck _collisionCheck;
+
+    private const float DEF_GRAVITY_SCALE = 1.0f;
+
+    void Awake() {
+        _body = GetComponent<Rigidbody2D>();
+        _collisionCheck = GetComponent<CollisionCheck>();
+    }
+
+    void Start() {
+        _coyoteCounter = _coyoteTime;
+        _wallStickCounter = _wallStickTime;
+    }
+
+    void Update() {
+        _moveX = Input.GetAxisRaw("Horizontal");
+        _projectedVelX = _moveX * Mathf.Max(_maxVel - _collisionCheck.friction, 0.0f);
+        _wantJump |= Input.GetButtonDown("Jump") && !_collisionCheck.isWall;
+        _wantWallJump |= Input.GetButtonDown("Jump") && (_collisionCheck.isWall && !_collisionCheck.isGround);
+    }
+
+    void FixedUpdate() {
+        _velocity = _body.velocity;
+        Debug.Log(_velocity.x);
+        playerMoveKeyboard();
+
+#region Coyote Counter
+        if (_collisionCheck.isGround && _body.velocity.y == 0) {
+            _coyoteCounter = _coyoteTime;
+        } else {
+            _coyoteCounter -= Time.deltaTime;
+        }
+#endregion
+
+#region Wall Stick Counter
+        if (_wallStickCounter > 0.0f && _collisionCheck.isWall && !_collisionCheck.isGround && !_isWallJumping) {
+            wallStick();
+        } else {
+            _wallStickCounter = _wallStickTime;
+        }
+#endregion
+
+#region Jump
+        if (_wantJump) {
+            jump();
+            _wantWallJump = false;
+        }
+#endregion
+
+#region Wall Jump
+        if (_wantWallJump) {
+            wallJump();
+            _wantJump = false;
+        }
+#endregion
+
+#region Wall Slide
+        if (_collisionCheck.isWall && _velocity.y < -_wallSlideMaxVel) {
+            _velocity.y = -_wallSlideMaxVel;
+        }
+#endregion
+
+        // adjust gravity scale
+        _body.gravityScale = _body.velocity.y == 0 ? DEF_GRAVITY_SCALE 
+            : _body.velocity.y > 0 ? _riseMult
+            : _fallMult;
+
+        _body.velocity = _velocity;
+    }
+
+    void playerMoveKeyboard() {
+        float acceleration = (_collisionCheck.isGround ? _maxAccel : _maxAirAccel) * Time.deltaTime;
+        _velocity.x = Mathf.MoveTowards(_velocity.x, _projectedVelX, acceleration);
+    }
+
+    void wallStick() {
+        _velocity.x = 0.0f;
+        if (_moveX == _collisionCheck.contactNormal.x) {
+            _wallStickCounter -= Time.deltaTime;
+        } else {
+            _wallStickCounter = _wallStickTime;
+        }
+    }
+
+    void jump() {
+        if (_coyoteCounter <= 0.0f && _jumpCount > _maxAirJumps) {
+            _wantJump = false;
+            return;
+        }
+        _jumpCount++;
+        _coyoteCounter = 0.0f;
+        float jumpVel = Mathf.Sqrt(-2f * Physics2D.gravity.y * _jumpHeight);
+        _velocity = new Vector2(_velocity.x, jumpVel);
+        _wantJump = false;
+    }
+
+    void wallJump() {
+        if (-_collisionCheck.contactNormal.x == _moveX) {
+            _velocity = new Vector2(_wallJumpClimb.x * _collisionCheck.contactNormal.x, _wallJumpClimb.y);
+        } else if (_collisionCheck.contactNormal.x == _moveX) {
+            _velocity = new Vector2(_wallJumpLeap.x * _collisionCheck.contactNormal.x, _wallJumpLeap.y);
+        }
+        _wantWallJump = false;
+        _isWallJumping = true;
+    }
+
+    void OnCollisionEnter2D() {
+        if (_collisionCheck.isWall && !_collisionCheck.isGround && _isWallJumping) {
+            _body.velocity = Vector2.zero;
+        }
+        if (_collisionCheck.isGround) {
+            _jumpCount = 0;
+        }
+        _isWallJumping = false;
+    }
+}
