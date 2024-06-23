f04ee843360e57f094000b73ba5769f62e6b8b35
Hashmat02 <157471281+Hashmat02@users.noreply.github.com>
2024-06-23 19:04:22 +0500

    Main Menue

--- Assets/Scripts/Mainmenue.cs
+++ Assets/Scripts/Mainmenue.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Mainmenue : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void GoToSettingsMenue()
+    {
+        SceneManager.LoadScene("SettingsMenue");
+    }
+    public void MainMenue()
+    {
+        SceneManager.LoadScene("Game Menue");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
