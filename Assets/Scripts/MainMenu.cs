40c5174b25f3f73ff945e84e1a30fda1faa667bf
Hashmat02 <157471281+Hashmat02@users.noreply.github.com>
2024-06-24 00:33:47 +0500

    Options Pannel
    -Game audio options panel UI added
    -Return Functionality from settings menu to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -19,7 +19,7 @@ public class MainMenu : MonoBehaviour
     }
     public void GoToMainMenu()
     {
-        SceneManager.LoadScene("GameMenu");
+        SceneManager.LoadScene("MainMenu");
     }
 
 
