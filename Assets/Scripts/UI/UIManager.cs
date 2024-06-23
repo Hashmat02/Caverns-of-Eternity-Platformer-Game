452577ebe0cce798f584c1252a672538eee08e08
HammadtJavaid <25100271@lums.edu.pk>
2024-06-24 04:56:00 +0500

    Hammad

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Game Over")]
+    [SerializeField] private GameObject gameOverScreen;
+    [SerializeField] private AudioClip gameOverSound;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
+
+    private void Awake()
+    {
+        gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // If pause screen already active, unpause and vice versa
+            PauseGame(!pauseScreen.activeInHierarchy);
+        }
+    }
+
+    #region Game Over
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+        // SoundManager.Instance.PlaySound(gameOverSound);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Game Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Exits play mode (will only be executed in the editor)
+#endif
+    }
+
+    #endregion
+
+    #region Pause
+
+    public void PauseGame(bool status)
+    {
+        pauseScreen.SetActive(status);
+        Time.timeScale = status ? 0 : 1; // Pause or resume the game
+    }
+
+    #endregion
+}
