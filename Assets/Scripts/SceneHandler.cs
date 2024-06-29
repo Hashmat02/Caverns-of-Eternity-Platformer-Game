b8e129ac29020484cc50b984c31e75838514581d
Hashmat02 <157471281+Hashmat02@users.noreply.github.com>
2024-06-29 20:06:58 +0500

    Pause Menu Button + Minor UI Changes
    -Fixed resume button mapping in pause menu
    -Pause button added to game level and mapped to pause menu
    -Removed arrow selection functionality for pause menu as it was really buggy
    -Minor UI fixes in game over menu + main menu
    
    #UI changes + consistent theme to all menus to be implemented yet

--- Assets/Scripts/SceneHandler.cs
+++ Assets/Scripts/SceneHandler.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneHandler : MonoBehaviour {
-    public static void GoToMainMenu() {
+public class SceneHandler : MonoBehaviour
+{
+    public static void GoToMainMenu()
+    {
         Time.timeScale = 1f;
         SceneManager.LoadScene(Constants.SCENE_MAIN_MENU);
     }
 
-    public static void GoToGameplay() {
+    public static void GoToGameplay()
+    {
         Time.timeScale = 1f;
         SceneManager.LoadScene(Constants.SCENE_GAMEPLAY);
     }
 
-	public static void ReloadScene() {
+    public static void ReloadScene()
+    {
         Time.timeScale = 1f;
-		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-	}
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
-    public static void QuitGame() {
+    public static void QuitGame()
+    {
 #if UNITY_EDITOR
-		UnityEditor.EditorApplication.ExitPlaymode();
-		return;
+        UnityEditor.EditorApplication.ExitPlaymode();
+        return;
 #endif
         Application.Quit();
     }
 }
+
+
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,3 +1,69 @@
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+//public class UIManager : MonoBehaviour
+//{
+//    [Header("Game Over")]
+//    [SerializeField] private GameObject gameOverScreen;
+
+//    [Header("Pause")]
+//    [SerializeField] private GameObject pauseScreen;
+
+//    public static UIManager instance;
+//    public delegate void OnGameOver();
+//    public static event OnGameOver onGameOver;
+
+//    void Awake()
+//    {
+//        if (instance && instance != this)
+//        {
+//            Destroy(this);
+//            return;
+//        }
+//        instance = this;
+
+//        if (!gameOverScreen)
+//        {
+//            ErrorHandling.throwError("No GameOverPanel found.");
+//        }
+//        if (!pauseScreen)
+//        {
+//            ErrorHandling.throwError("No PausePanel found.");
+//        }
+//    }
+
+//    private void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.Escape))
+//        {
+//            // If pause screen already active, unpause and vice versa
+//            PauseGame(!pauseScreen.activeInHierarchy);
+//        }
+//    }
+
+//    #region Game Over
+
+//    public void GameOver()
+//    {
+//        Time.timeScale = 0f;
+//        gameOverScreen.SetActive(true);
+//        onGameOver?.Invoke();
+//    }
+
+//    #endregion
+
+//    #region Pause
+
+//    public void PauseGame(bool status)
+//    {
+//        pauseScreen.SetActive(status);
+//        Time.timeScale = status ? 0 : 1; // Pause or resume the game
+//    }
+
+//    #endregion
+//}
+
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,31 +75,37 @@ public class UIManager : MonoBehaviour
     [Header("Pause")]
     [SerializeField] private GameObject pauseScreen;
 
-	public static UIManager instance;
-	public delegate void OnGameOver();
-	public static event OnGameOver onGameOver;
+    public static UIManager instance;
+    public delegate void OnGameOver();
+    public static event OnGameOver onGameOver;
 
-	void Awake() {
-		if (instance && instance != this) {
-			Destroy(this);
-			return;
-		}
-		instance = this;
+    private bool isPaused = false; // Track if the game is currently paused
 
-		if (!gameOverScreen) {
-			ErrorHandling.throwError("No GameOverPanel found.");
-		}
-		if (!pauseScreen) {
-			ErrorHandling.throwError("No PausePanel found.");
-		}
-	}
+    void Awake()
+    {
+        if (instance && instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        instance = this;
+
+        if (!gameOverScreen)
+        {
+            ErrorHandling.throwError("No GameOverPanel found.");
+        }
+        if (!pauseScreen)
+        {
+            ErrorHandling.throwError("No PausePanel found.");
+        }
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // If pause screen already active, unpause and vice versa
-            PauseGame(!pauseScreen.activeInHierarchy);
+            // Toggle pause state
+            TogglePause();
         }
     }
 
@@ -41,19 +113,40 @@ public class UIManager : MonoBehaviour
 
     public void GameOver()
     {
-		Time.timeScale = 0f;
+        Time.timeScale = 0f;
         gameOverScreen.SetActive(true);
-		onGameOver?.Invoke();
+        onGameOver?.Invoke();
     }
 
     #endregion
 
     #region Pause
 
-    public void PauseGame(bool status)
+    public void TogglePause()
     {
-        pauseScreen.SetActive(status);
-        Time.timeScale = status ? 0 : 1; // Pause or resume the game
+        isPaused = !isPaused; // Toggle pause state
+
+        if (isPaused)
+        {
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0; // Pause the game
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("resumeivoided");
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1; // Resume the game
     }
 
     #endregion
